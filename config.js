796b0c70e52be47db05112050ccf92b19ac0b5f5
WuYuZi <hz@25ic.com>
2017-06-08 10:43:40 +0800

    s

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var config = {
 	    //填写数据库连接信息，可查询数据库详情页
         username:'root',//mysql数据库管理员
         password:'123456',//密码
-        db_host:'127.0.0.1',//主机
+        db_host:'122.114.180.147',//主机
         db_port: 3306,//端口
 	    db_name:'myvideo'//数据库名
     },
